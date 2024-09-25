090a82585e1047a198a915888ce74d917367bd4d
gomess1234 <142537724+gomess1234@users.noreply.github.com>
2024-09-25 08:11:03 -0300

    injjkk km,

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function soma (numero1,numero2){
 }
 console.log (soma(5,2));
 
-function área do quadrado (numero1,numero2){
-    return numero1+numero2;
+function areaQuadrado (lado){
+    return lado**2;
 }
-console.log (área do quadrado(6,3));
+console.log (áreaQuadrado(6));
