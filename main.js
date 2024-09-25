557a581d94270b59d1ccf5481421424b5e860214
gomess1234 <142537724+gomess1234@users.noreply.github.com>
2024-09-25 08:02:20 -0300

    fhnjfnfh

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function soma (numero1,numero2){
     return numero1+numero2;
 }
 console.log (soma(5,2));
+
+function área do quadrado (numero1,numero2){
+    return numero1+numero2;
+}
+console.log (área do quadrado(6,3));
