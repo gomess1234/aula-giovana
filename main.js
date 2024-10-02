a469b60f479ed80b5303f38dd4022e2eb2f2c821
gomess1234 <142537724+gomess1234@users.noreply.github.com>
2024-10-02 08:27:42 -0300

    fvdvfddf

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
-function soma (numero1,numero2){
-    return numero1+numero2;
-}
-console.log (soma(5,2));
+// function soma (numero1,numero2){
+//     return numero1+numero2;
+// }
+// console.log (soma(5,2));
 
-function áreaQuadrado (lado){
-    return lado**2;
-}
-console.log (áreaQuadrado (6));
-let media= function( ) {
-    return ;
-}
-console.log (media(1,2,3) );
+// function áreaQuadrado (lado){
+//     return lado**2;
+// }
+// console.log (áreaQuadrado (6));
+// let media= function( ) {
+//     return ;
+// }
+// console.log (media(1,2,3) );
 
-let nome=prompt(nome?) 
-document.write (nome);
-=parseInt(prompt (nome?));
+// let nome=prompt(nome?) 
+// document.write (nome);
+// =parseInt(prompt (nome?));
+let 
+estados =['pr','rj','sp','sc','mg','bh','ba','df','es','ma','mt','ms','pb','pa','pe','pi','rn','rs','ro','rr','se','to','ac','al','ap','am','ce'];
+let i=0;
+while ( i < estados.length ){
+console.log(estados[i]);
+i=i+1;
+}
+for(let i=0; i<estados.length;i++) {
+console.log(i);
+console.log(estados[i]);
+
+}
+let idade = parseInt (prompt ('Qual a sua idade?'));
+if (idade>17){
+console.log ('pode dirigir');
+}
+else {
+console.log('ilegal dirigir');
+}
